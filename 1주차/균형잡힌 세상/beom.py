22ade2ddeefe1b2c158baa29836faeb67742ffe8
beom <habem3618@naver.com>
2021-08-05 01:26:55 +0900

    Add beom

---
+++ 1주차/균형잡힌 세상/beom.py
@@ -0,0 +1,31 @@
+while True:
+    
+    texts = input()
+    result = []
+
+    if texts == '.':
+        break
+
+    for text in texts:
+
+        if text == '(' or text == '[':
+            result.append(text)
+
+        elif text == ')':
+            if result and result[-1] == '(':
+                result.pop()
+            else:
+                result.append(text)
+                break
+
+        elif text == ']':
+            if result and result[-1] == '[':
+                result.pop()
+            else:
+                result.append(text)
+                break
+    
+    if result:
+        print('no')
+    else:
+        print('yes')    
---
+++ 1주차/큐 2/beom.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+T = int(sys.stdin.readline())
+queue = deque([])
+
+for tc in range(T):
+    order = sys.stdin.readline().split()
+
+    if order[0] == 'push':
+        queue.append(order[1])
+
+    if order[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    
+    if order[0] == 'size': 
+        print(len(queue))  # 직접 짜는 것보다 len이 더 빠르다??
+    
+    if order[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+
+    if order[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+
+    if order[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
