8ca12fcaf6ddc1f8a0674859dc76838991eb7693
hyeongjunkim <hjthelegend@naver.com>
2021-08-10 20:41:07 +0900

    add edit

--- 2주차/회전하는 큐/hyeongjun.py
+++ 2주차/회전하는 큐/hyeongjun.py
@@ -1,57 +1,64 @@
-# 길이가 N으로 같은 문자열 X, Y
-# 두 문자열의 차이는 X[i] != Y[i]인 i의 개수.
-# X = 'jimin'
-# Y = 'minji'
-# 둘의 차이는 4 -> 2번째 i가 동일함. "i"
+# 10
+# 2 9 5
+# 1 2 3 4 5 6 7 8 9 10 -> 2와 9와 5를 빼야한다 (처음 큐에서의 위치)
+# /2 3 4 5 6 7 8 9 10 1
+# 3 4 5 6 7 8 9 10 1
+# /1 3 4 5 6 7 8 9 10
+# /10 1 3 4 5 6 7 8 9
+# /9 10 1 3 4 5 6 7 8
+# 10 1 3 4 5 6 7 8
+# /1 3 4 5 6 7 8 10
+# /3 4 5 6 7 8 10 1
+# /4 5 6 7 8 10 1 3
+# /5 6 7 8 10 1 3 4
+# 6 7 8 10 1 3 4
+# -> 총 8번
 
-# len(a) <= len(b)
-# 1. a 앞에 아무 알파벳 추가
-# 2. a 뒤에 아무 알파벳 추가
-# len(a) == len(b) and 차이를 최소화
-
-# adaabc
-# aababbc
-# /aadaabc
-# /aababbc
-# 2 -> 앞에 추가, 이게 답
-# /adaabcc
-# /aababbc
-# 3 -> 뒤에 추가, 차이가 더 큼
-
-# aaaa
-# bbbbaaaa
-# /bbbbaaaa
-# /bbbbaaaa
-# 4번 추가를 했지만 차이는 0이다.
-# /aaaaaaaa
-# /bbbbaaaa
-# 뒤에 추가를 하니 4차이가 4임
-
-# aaa
-# aaaabbbb 만약 이렇게 있다면
-
-# aaa0000
-# aaaabbb -> 차이는 0
-
-# 0aaa000
-# aaaabbb -> 차이는 0
-
-# 00aaa00
-# aaabbbb -> 차이는 2
-
-# 처음에 이런 식으로 가장 차이가 적은 것을 찾으면 되겠다.
-
+from collections import deque
+import copy
 import sys
 
-a, b = sys.stdin.readline().split()
-count_list = []
+n, m = map(int, sys.stdin.readline().split())
+finds = map(int, sys.stdin.readline().split())
 
+ready = deque([i for i in range(1, n+1)])
+result = 0
 
-for i in range(len(b) - len(a)+1): # 차이만큼 반복 할 수 있도록 # +1 추가, 앞 뒤로 2번반복 해야함
-    count = 0
-    for j in range(len(a)): # a는 계속 반복 시키면서 비교
-        if a[j] != b[i+j]:
-            count += 1
-    count_list.append(count)
+for x in finds:
+    ready2 = copy.copy(ready) # 제대로 복사하기 위함. (copy.deepcopy)런타임 에러 (copy.copy) 통과
+    ready3 = copy.copy(ready)
+    count2 = 0
+    count3 = 0
+    for i in range(len(ready)): # 왼쪽으로 가는 2번
+        if ready2[0] == x:
+            ready2.popleft()
+            # print(ready)
+            break
+        
+        popped = ready2.popleft()
+        ready2.append(popped)
+        count2 += 1
 
-print(min(count_list))
+    for i in range(len(ready)): # 오른쪽으로 가는 3번
+        if ready3[0] == x:
+            ready3.popleft()
+            # print(ready)
+            break
+
+        popped = ready3.pop()
+        ready3.appendleft(popped)
+        count3 += 1
+
+    # 두 가지 경우 수를 생각해야하기 때문에 count가 낮으면 그 ready(숫자) 큐를 ready(기본)에 할당한다. 
+    if count2 < count3:
+        ready = ready2
+        result += count2
+    else:
+        ready = ready3
+        result += count3
+
+    # print(count2)
+    # print(count3)
+    # print(result)
+    
+print(result)ㄴ
