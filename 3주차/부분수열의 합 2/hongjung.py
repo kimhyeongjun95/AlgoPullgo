fc807d8ffbe9f38edcc97cd88be083b72f26ad2d
HongJungKim <hong267@naver.com>
2021-08-14 13:33:34 +0900

    Update [1208]/[hongjung.py]

--- 3주차/부분수열의 합 2/hongjung.py
+++ 3주차/부분수열의 합 2/hongjung.py
@@ -1,18 +1,34 @@
-import sys # 시간초과
+# import sys 시간초과
 
-N, S = map(int, sys.stdin.readline().split())
-numbers = list(map(int, sys.stdin.readline().split()))
+# N, S = map(int, sys.stdin.readline().split())
+# numbers = list(map(int, sys.stdin.readline().split()))
 
-n = len(numbers)
+# n = len(numbers)
 
-cnt = 0
-for i in range(1, 1 << n):
-    tmp = []
-    for j in range(n):
-        if i & (1 << j):
-            tmp.append(numbers[j])
-    print(tmp)
-    # if tmp == S:
-    #     cnt += 1
+# cnt = 0
+# for i in range(1, 1 << n):
+#     tmp = []
+#     for j in range(n):
+#         if i & (1 << j):
+#             tmp.append(numbers[j])
+#     if sum(tmp) == S:
+#         cnt += 1
+
+# print(cnt)
+
+
+# import sys 시간초과
+# from itertools import combinations
+
+# N, S = map(int, sys.stdin.readline().split())
+# numbers = list(map(int, sys.stdin.readline().split()))
+
+# n = len(numbers)
+
+# cnt = 0
+# for i in range(n+1):
+#     for j in combinations(numbers, i):
+#         if j != () and sum(j) == S:
+#             cnt += 1
 
 # print(cnt)
