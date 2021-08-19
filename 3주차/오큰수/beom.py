bf24391fb1ff876c4ca590680c16a45a305f306d
beom <habem3618@naver.com>
2021-08-19 18:17:43 +0900

    Add beom

---
+++ 3주차/오큰수/beom.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+# 1
+
+def find_NGE(arr, N):
+    result = [-1] * N
+    
+    for i in range(N):
+        stack = []
+        j = 1
+        while i + j < N:
+            if len(stack) == 0 and arr[i] < arr[i+j]:
+                stack.append(arr[i+j])
+                result[i] = stack[-1]
+            j += 1
+    return result
+
+N = int(input())                         
+arr = list(map(int, input().split()))  
+print(*find_NGE(arr, N))
+    
+
+# 2
+
+# N = int(input())                         
+# arr = list(map(int, input().split()))  
+# stack = []
+# result = [-1] * N
+
+# for i in range(N):    # i = 0 1 2 3
+#     j = 1
+#     while i + j < N:
+#         if arr[i] < arr[i+j]:
+#             result[i] = arr[i+j]
+#             break    
+#         j += 1
+# print(*result)
+  
+
---
+++ 3주차/잃어버린 괄호/beom.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+arr = input().split('-')
+result = []
+
+for i in arr:
+    total = 0
+    arr_1 = i.split('+')
+    
+    for j in arr_1:
+        total += int(j)  
+    result.append(total)
+ 
+num = result[0]
+for i in range(1, len(result)):
+    num -= result[i]
+print(num)
+
