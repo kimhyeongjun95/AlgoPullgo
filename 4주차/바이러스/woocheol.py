d7e35ebfb9aeca26869e5beb4a0b9ce910df2115
woosteelz <woosteelz@naver.com>
2021-08-24 21:44:54 +0900

    MODIFY [week4/woocheol]

--- 4주차/바이러스/woocheol.py
+++ 4주차/바이러스/woocheol.py
@@ -1,25 +1,24 @@
 # 백준 문제번호 2606번 바이러스
-
 import sys
 from collections import deque
 
 N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
-graph = []
+graph = [[] for _ in range(N+1)]
 visited = [False for _ in range(N+1)]
 for _ in range(M):
     a, b = map(int, sys.stdin.readline().split())
-    graph.append([a, b])
-    graph.append([b, a])
+    graph[a].append(b)
+    graph[b].append(a)
 
 queue = deque()
 queue.append(1)
 
 while queue:
-    temp = queue.popleft()
-    visited[temp] = True
-    for a, b in graph:
-        if a == temp and not visited[b]:
-            queue.append(b)
+    curr = queue.popleft()
+    visited[curr] = True
+    for n in graph[curr]:
+        if not visited[n]:
+            queue.append(n)
 
 print(visited.count(True)-1)
