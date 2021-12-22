4a07e65b7800bffbedf90c6b7a50570743ca6c07
HongJungKim <hong267@naver.com>
2021-12-22 18:23:49 +0900

    update night work / hongjung.py

--- 021주차/야근 지수/hongjung.py
+++ 021주차/야근 지수/hongjung.py
@@ -1,18 +1,42 @@
-# 효율성 테스트 성공 못함
-def solution(n, works):
+import heapq
 
-    while n > 0:
-        works[works.index(max(works))] -= 1
-        if sum(works) == 0:
-            break
-        n -= 1
-        
-    answer = 0
+def solution(n, works):
+    heap = []
     for w in works:
-        answer += w**2
+        heapq.heappush(heap, (-w, w))
+    
+    while n > 0:
+        tmp = heapq.heappop(heap)[1]
+        if tmp == 0:
+            break
+        tmp -= 1
+        n -= 1
+        heapq.heappush(heap, (-tmp, tmp))
+        if n == 0:
+            break
+    
+    answer = 0
+    for h in heap:
+        answer += h[1] ** 2
+        
     return answer
 
 
 print(solution(4, [4, 3, 3]))
 print(solution(1, [2, 1, 2]))
 print(solution(3, [1, 1]))
+
+
+# 효율성 테스트 성공 못함
+# def solution(n, works):
+
+#     while n > 0:
+#         works[works.index(max(works))] -= 1
+#         if sum(works) == 0:
+#             break
+#         n -= 1
+        
+#     answer = 0
+#     for w in works:
+#         answer += w**2
+#     return answer
